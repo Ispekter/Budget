8f6e36bbe0e8b0736432eb7943ac6b14025ef74c
Ispekter <viktor@dtpservice.eu>
2024-05-02 22:39:28 +0300

    Added Budget Data

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,7 @@
 export const fetchData = (key) => {
-    return localStorage.getItem(key)
-  }
+  return localStorage.getItem(key)
+}
+
+export const postData = (key, value) => {
+	return localStorage.setItem(key, value)
+}
