a0a90b1937d9084ac0cb7a62733d786de12fde26
Ispekter <viktor@dtpservice.eu>
2024-05-02 19:32:01 +0300

    Added comment to index.css

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Outlet } from 'react-router-dom'
 import wave from '../assets/wave.svg'
 import Nav from '../components/Nav'
 
+// Test Comment
+
 const Main = () => {
   return (
 	<div className="layout">
